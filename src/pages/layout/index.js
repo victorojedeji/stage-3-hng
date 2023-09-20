4dc9ba93720b9493882ea1fbdab3501fafaca50e
victorojedeji <ojedejivictordaniel@gmail.com>
2023-09-21 00:15:10 +0100

    stage-3-hng

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,12 +1,12 @@
 import { useEffect } from 'react'
 import {useNavigate } from 'react-router-dom';
-import { GALLERY } from '../../lib/routes';
+import { LOGIN } from '../../lib/routes';
 
 export default function IndexPage() {
   const navigate = useNavigate();
 
   useEffect(() => {
-      navigate(GALLERY);
+      navigate(LOGIN);
   }, []);
 
   return (
